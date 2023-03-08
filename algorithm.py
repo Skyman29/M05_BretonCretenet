33d9a083458e04e425f369d22140fa25f5e46dc4
Skyman29 <gaetan.breton@gmail.com>
2023-03-08 15:43:20 +0100

    ADD algorithm.py file created

---
+++ algorithm.py
@@ -0,0 +1,11 @@
+# algorithm
+
+# This module contains all the function in order to build the machine learning algorithm of the project
+
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.tree import DecisionTreeRegressor
